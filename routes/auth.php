43b60aece65ff5cf90e25dd8db7aad30bb3724a1
nuu <wisnu@gmail.com>
2026-02-06 22:09:35 +0700

    setup daisy + livewire

---
+++ routes/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', App/Livewire/Auth/Login::class)->name('login');
+});
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Require __DIR__.'/auth.php';
